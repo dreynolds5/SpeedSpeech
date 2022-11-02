18d85c7a832159966d7e589c1aa36052fb21613f
danny <dannyreynolds6404@gmail.com>
2022-11-02 09:10:05 -0700

    added method to change stop variable

--- src/main/java/SpeechToTextFromMicrophone.java
+++ src/main/java/SpeechToTextFromMicrophone.java
@@ -1,42 +1,12 @@
-
-import com.google.api.gax.longrunning.OperationFuture;
-import com.google.api.gax.longrunning.OperationTimedPollAlgorithm;
-import com.google.api.gax.retrying.RetrySettings;
-import com.google.api.gax.retrying.TimedRetryAlgorithm;
-import com.google.api.gax.rpc.ApiStreamObserver;
-import com.google.api.gax.rpc.BidiStreamingCallable;
 import com.google.api.gax.rpc.ClientStream;
 import com.google.api.gax.rpc.ResponseObserver;
 import com.google.api.gax.rpc.StreamController;
-import com.google.cloud.speech.v1.LongRunningRecognizeMetadata;
-import com.google.cloud.speech.v1.LongRunningRecognizeResponse;
-import com.google.cloud.speech.v1.RecognitionAudio;
-import com.google.cloud.speech.v1.RecognitionConfig;
-import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
-import com.google.cloud.speech.v1.RecognizeResponse;
-import com.google.cloud.speech.v1.SpeechClient;
-import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
-import com.google.cloud.speech.v1.SpeechRecognitionResult;
-import com.google.cloud.speech.v1.SpeechSettings;
-import com.google.cloud.speech.v1.StreamingRecognitionConfig;
-import com.google.cloud.speech.v1.StreamingRecognitionResult;
-import com.google.cloud.speech.v1.StreamingRecognizeRequest;
-import com.google.cloud.speech.v1.StreamingRecognizeResponse;
-import com.google.cloud.speech.v1.WordInfo;
-import com.google.common.util.concurrent.SettableFuture;
+import com.google.cloud.speech.v1.*;
 import com.google.protobuf.ByteString;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
+
+import javax.sound.sampled.*;
 import javax.sound.sampled.DataLine.Info;
-import javax.sound.sampled.TargetDataLine;
+import java.util.ArrayList;
 public class SpeechToTextFromMicrophone {
     //backend class that runs google speech-text API
     /* Parts of this code falls under the copyright information listed below
@@ -158,5 +128,14 @@ public SpeechToTextFromMicrophone(){
         }
         responseObserver.onComplete();
     System.out.println(transcription);
+    }
+
+    public void swapStop(){
+        if (!stop){
+            stop = true;
+        }
+        else {
+            stop = false;
+        }
     }
 }
