3ee108f2fa2f05c43160990fcd69962aaed35344
raynamehta <104454210+raynamehta@users.noreply.github.com>
2022-11-29 09:52:55 -0500

    Update pageTwo.java

--- src/main/java/pageTwo.java
+++ src/main/java/pageTwo.java
@@ -35,7 +35,7 @@ public class pageTwo {
             label = new JLabel();
             label.setBounds(150, 100, 400, 400);
             label.setVisible(false);
-            label.setText("Preferences");
+            label.setText("How To");
             label.setHorizontalTextPosition(JLabel.CENTER);
             label.setFont(new Font("Helvetica", Font.BOLD, 50));
             label.setVisible(true);
@@ -43,9 +43,9 @@ public class pageTwo {
             label2 = new JLabel();
             label2.setBounds(225, 200, 400, 400);
             label2.setVisible(false);
-            label2.setText("How Long?");
-            label2.setHorizontalTextPosition(JLabel.CENTER);
-            label2.setFont(new Font("Helvetica", Font.BOLD, 25));
+            label2.setText("When you press the Record button, you will be directed to the Record page, with a timer, Start button, and Stop button. Press the Start button when you are ready to give your speech and the Stop button when you want the timer to stop. After a few moments, you will be directed to the Summary analysis page.");
+            label2.setHorizontalTextPosition(JLabel.LEFT);
+            label2.setFont(new Font("Helvetica", Font.BOLD, 15));
             label2.setVisible(true);
 
 
