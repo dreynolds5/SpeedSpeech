8ff7b0c7c5c959315d1b91fc47abd062740f2926
danny <dannyreynolds6404@gmail.com>
2022-12-05 10:00:51 -0700

    added a couple of tips on speaking

--- src/main/java/PageHowTo.java
+++ src/main/java/PageHowTo.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 public class PageHowTo extends JPanel implements ActionListener {
 	private JLabel label;
 	private JTextArea label2;
-
+	private JTextArea label3;
 	private JButton button3;
 	//private JLabel label_name;
 
@@ -32,10 +32,11 @@ public class PageHowTo extends JPanel implements ActionListener {
 		label2.setLineWrap(true);
 		label2.setWrapStyleWord(true);
 		label2.setText(
-				"When you press the Record button, you will be directed to the Record page, with a timer, Start button, and Stop button. Press the Start button when you are ready to give your speech and the Stop button when you want the timer to stop. After a few moments, you will be directed to the Summary analysis page.");
+				"When you press the Record button, you will be directed to the Record page, with a timer, Start button, and Stop button. Press the Start button when you are ready to give your speech and the Stop button when you want the timer to stop. After a few moments, you will be directed to the Summary analysis page. \n\n\n Tips For Speaking \n 1. Practice before hand \n 2. Work the room, make eye contact with people \n 3. Prepare with relaxation techniques \n 4. Don't read the speech \n 5. Have good posture \n\n\n\n Tips about speaking from https://sps.columbia.edu/news/five-tips-give-great-speech");
 		//label2.setHorizontalTextPosition(JLabel.LEFT);
 		label2.setFont(new Font("Helvetica", Font.BOLD, 15));
 		label2.setVisible(true);
+
 
 		this.setLayout(null);
 		this.setSize(window.getWidth(), window.getHeight());
